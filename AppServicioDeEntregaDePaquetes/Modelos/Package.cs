5be9524afb3fac0d5bc5e3c788fb09976b9c8c27
lguerr <leonidas_jose_guerrero@hotmail.com>
2015-03-09 18:12:58 -0500

    Parcial

---
+++ AppServicioDeEntregaDePaquetes/Modelos/Package.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Web;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+using System.ComponentModel;
+
+namespace AppServicioDeEntregaDePaquetes
+{
+    public abstract class Package : OvernightPackage
+    {
+        private  string nombre;
+        private  int codigo ;
+        private  int direccion ;
+        private  string cuidad ;
+        private string Departamento ;
+        private string codigo_postal_remitente;
+        private string codigo_postal_destinatario;
+        private float peso ;
+        private float costo;
+
+        public Package(string nombre;)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
